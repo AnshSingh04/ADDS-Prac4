8dd4cdbdb92115b73b78cca456b85c837372f3e2
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-04-03 21:53:37 +0930

    Add files via upload

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -5,8 +5,8 @@ Truckloads::Truckloads() {
 }
 
 int Truckloads::numTrucks(int numCrates, int loadSize) {
-	if(numCrates<=0 || loadSize<0)
-		return -1;
+	if(numCrates<=0 || loadSize<=0)
+    	return 0;
 	if(numCrates<=loadSize)
 		return 1;
 	if(numCrates%2)
