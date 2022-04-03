f529e8e77a678926eeefec123712391124cbfa52
AnshSingh04 <73479734+AnshSingh04@users.noreply.github.com>
2022-04-03 19:43:26 +0930

    Add files via upload

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 #include <math.h>
 EfficientTruckloads::EfficientTruckloads() {
+    sum=0;
 }
 
-int EfficientTruckloads::numTrucks(int numCrates, int loadSize, int sum) {
+int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
     if(numCrates<=loadSize)
 		return 1;
 	if(numCrates%2) {
-        sum += numTrucks(numCrates/2 + 1, loadSize,sum)+numTrucks(numCrates/2, loadSize,sum);
+        sum = numTrucks(numCrates/2 + 1, loadSize)+numTrucks(numCrates/2, loadSize);
         return sum;
     }
-	sum += 2*numTrucks(numCrates/2, loadSize,0);
+	sum = 2*numTrucks(numCrates/2, loadSize);
     return sum;
 }
